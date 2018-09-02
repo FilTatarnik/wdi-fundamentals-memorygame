8e8666515ac1da9056dbac24069ff8fced7088c4
Filip Tatarnik <ftatarnik@gmail.com>
2018-09-02 12:12:38 -0500

    created game logic i think

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,45 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+
+//card objects
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage:"images/queen-of-hearts.png"
+	},
+		{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage:"images/queen-of-diamonds"
+	},
+		{
+		rank: "king",
+		suit: "hearts",
+		cardImage:"images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
+
+//variable that checks if the card is in play i guess. or the logic
+
 var cardsInPlay = [];
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+		} else {
+		console.log("Sorry, try again.");
+}
+};
+
+
+//flipcard function
 var flipCard = function(cardId){
 
 
-if (cardsInPlay[0] === cardsInPlay[1]){
+if (cardsInPlay[0] === cardsInPlay[2]){
 
 	alert("You've found a match!");
 
@@ -11,11 +47,21 @@ if (cardsInPlay[0] === cardsInPlay[1]){
 	alert("Sorry, try again.");
 
 	
-	};
-	console.log("User flipped " + cards[cardId]);
+	}
+	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId]);
+
+
+
+	console.log([cards[cardId].cardImage]);
+	console.log([cards[cardId].suit]);
+
+
+
+	// checkForMatch();
 };
 
+//calling the flipCard function
 flipCard(0);
 
 flipCard(2);
